94a62e6bb1c1eda2a8de04f10d3c2832f97c5047
yunweiyuan <yunwei.yuan@alumni.usc.edu>
2018-01-19 21:45:18 -0800

    update 486.predict-the-winner.java

--- 486.predict-the-winner.java
+++ 486.predict-the-winner.java
@@ -52,7 +52,7 @@
  */
 class Solution {
     public boolean PredictTheWinner(int[] nums) {
-		int[][] dp = new int[nums.length][nums.length];
+/*		int[][] dp = new int[nums.length][nums.length];
 		for(int i=0; i<nums.length; i++) dp[i][i] = nums[i];
 		for(int len=1; len<nums.length; len++){
 			for(int i=0; i<nums.length-len; i++){
@@ -61,5 +61,17 @@ class Solution {
 			}
 		}
 		return dp[0][nums.length-1] >=0;
+*/
+		if(nums == null) return true;
+		int n = nums.length;
+		if((n&1) == 0) return true;
+		int[] dp = new int[n];
+		for(int i=n-1; i>=0; i--){
+			for(int j=i; j<n; j++){
+				if(i==j) dp[i] = nums[i];
+				else dp[j] = Math.max(nums[i]-dp[j], nums[j]-dp[j-1]);
+			}
+		}
+		return dp[n-1]>=0;
     }
 }
