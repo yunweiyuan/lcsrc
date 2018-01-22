0953988e30bbfe8deeb340a0fd765646ba8d67eb
yunweiyuan <yunwei.yuan@alumni.usc.edu>
2018-01-21 22:01:03 -0800

    update 63.unique-paths-ii.java

---
+++ 63.unique-paths-ii.java
@@ -0,0 +1,45 @@
+/*
+ * [63] Unique Paths II
+ *
+ * https://leetcode.com/problems/unique-paths-ii/description/
+ *
+ * algorithms
+ * Medium (32.04%)
+ * Total Accepted:    124.7K
+ * Total Submissions: 388.8K
+ * Testcase Example:  '[[0]]'
+ *
+ * Follow up for "Unique Paths":
+ * 
+ * Now consider if some obstacles are added to the grids. How many unique paths
+ * would there be?
+ * 
+ * An obstacle and empty space is marked as 1 and 0 respectively in the grid.
+ * 
+ * For example,
+ * There is one obstacle in the middle of a 3x3 grid as illustrated below.
+ * 
+ * [
+ * ⁠ [0,0,0],
+ * ⁠ [0,1,0],
+ * ⁠ [0,0,0]
+ * ]
+ * 
+ * The total number of unique paths is 2.
+ * 
+ * Note: m and n will be at most 100.
+ */
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+		int w = obstacleGrid[0].length;
+		int[] dp = new int[w];
+		dp[0] = 1;
+		for(int[] row : obstacleGrid){
+			for(int i=0; i<w; i++){
+				if(row[i] == 1) dp[i] = 0;
+				else if(i > 0) dp[i] += dp[i-1];
+			}
+		}
+		return dp[w-1];
+    }
+}
