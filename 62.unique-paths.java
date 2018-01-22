e5e38737ee5705508a572fafd9eff86c856d679b
yunweiyuan <yunwei.yuan@alumni.usc.edu>
2018-01-21 20:57:29 -0800

    update 62.unique-paths.java

---
+++ 62.unique-paths.java
@@ -0,0 +1,40 @@
+/*
+ * [62] Unique Paths
+ *
+ * https://leetcode.com/problems/unique-paths/description/
+ *
+ * algorithms
+ * Medium (42.18%)
+ * Total Accepted:    173.6K
+ * Total Submissions: 410.2K
+ * Testcase Example:  '1\n2'
+ *
+ * A robot is located at the top-left corner of a m x n grid (marked 'Start' in
+ * the diagram below).
+ * 
+ * The robot can only move either down or right at any point in time. The robot
+ * is trying to reach the bottom-right corner of the grid (marked 'Finish' in
+ * the diagram below).
+ * 
+ * How many possible unique paths are there?
+ * 
+ * 
+ * 
+ * Above is a 3 x 7 grid. How many possible unique paths are there?
+ * 
+ * 
+ * Note: m and n will be at most 100.
+ */
+class Solution {
+    public int uniquePaths(int m, int n) {
+		int[][] dp = new int[m][n];
+		for(int i=0; i<m; i++) dp[i][0] = 1;
+		for(int i=0; i<n; i++) dp[0][i] = 1;
+		for(int i=1; i<m; i++){
+			for(int j=1; j<n; j++){
+				dp[i][j] = dp[i][j-1] + dp[i-1][j];
+			}
+		}
+		return dp[m-1][n-1];
+    }
+}
